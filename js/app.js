9fa02cbe2bee2841d4ebf68ca1b3d9b1f80ebcc2
Mario Macan <mariomacan@MacBook-Pro-2.local>
2024-03-28 10:09:31 +0100

    Input error image now works

--- js/app.js
+++ js/app.js
@@ -15,17 +15,24 @@ form.addEventListener("submit", e=>{
 function validateInput(){   
     for(const input of allInputs){
         const error=input.nextElementSibling;
+        const img=document.createElement("IMG");
+        img.src="images/icon-error.svg"
+        
         if(input.id==="email"){
             const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
             if(!re.test(email.value)){
                 input.placeholder="email@example/com"
                 error.innerText=`Looks like this is not an email`
                 input.classList.add("error-border");
-            }
+                input.style.background = "url(images/icon-error.svg) no-repeat 95%";
+
+
+            }   
             else{
                 input.value=email.value;
                 error.innerText="";
                 input.classList.remove("error-border");
+                input.style.background ="";
             }
         }
         
@@ -33,14 +40,16 @@ function validateInput(){
 
         else{
             if(input.value===""){
-                input.value=""
+                input.placeholder=""
                 error.innerText=`${input.name} cannot be empty`
                 input.classList.add("error-border");
+                input.style.background = "url(images/icon-error.svg) no-repeat 95%";
             }
             else{
                 input.value=input.value;
                 error.innerText="";
                 input.classList.remove("error-border");
+                input.style.background ="";
             }
         }
         
